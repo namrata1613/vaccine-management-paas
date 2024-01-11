dda81a88a6ecee26444d14ce40a11204b4b6f359
namrata1613 <dasnamrata2402@gmail.com>
2024-01-12 00:46:12 +0530

    reconfigure database

--- db.py
+++ db.py
@@ -11,11 +11,11 @@ db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
 
 def open_connection():
-    unix_socket = '/cloudsql/{}'.format('oecs1-cc-06:us-central1:patient-info-001')
+    unix_socket = '/cloudsql/{}'.format('oecs1-cc-06:us-central1:vaccine-mangement-info-001')
     try:
         if os.environ.get('GAE_ENV') == 'standard':
-            conn = pymysql.connect(user='namrata', password='namrata',
-                                unix_socket=unix_socket, db='patients',
+            conn = pymysql.connect(user='USER', password='PASSWORD',
+                                unix_socket=unix_socket, db='vaccine-management',
                                 cursorclass=pymysql.cursors.DictCursor
                                 )
     except pymysql.MySQLError as e:
@@ -24,25 +24,46 @@ def open_connection():
     return conn
 
 
+def insert_diagnosed(info):
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        cursor.execute('INSERT INTO Diagnosed (RTPCRId,EpId,LeavingDate,Hospital) VALUES(%s,%s,%s,%s)', (info["RTPCRId"], info["EpId"], info["LeavingDate"],info["Hospital"]))
+    conn.commit()
+    conn.close()
+
+
+
+def insert_regularCheckup(info):
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        cursor.execute('INSERT INTO RegCheckup (EpId,Result) VALUES(%s,%s)', (info["EpId"], info["Result"]))
+    conn.commit()
+    conn.close()
+
+def insert_vaccination(info):
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        cursor.execute('INSERT INTO vaccination (CertNo,vaccineName,Id,FirstVaccineDate,SecondVaccineDate) VALUES(%s,%s,%s,%s,%s)', (info["CertNo"], info["vaccineName"],info["Id"],info["FirstVaccineDate"],info["SecondVaccineDate"]))
+    conn.commit()
+    conn.close()
+
 
 def get_info():
     conn = open_connection()
     with conn.cursor() as cursor:
-        result = cursor.execute('SELECT * FROM info;')
+        result = cursor.execute('SELECT * FROM (select * from vaccination join employee on Id=EpId) union (select * vaccination join student on Id=SId);')
         info = cursor.fetchall()
         if result > 0:
             # Convert the results to a list of dictionaries
             info_list = []
             for row in info:
                 row_dict = {
-                    'id': row[0],
-                    'name': row[1],
-                    'gender': row[2],
-                    'age': row[3],
-                    'address': row[4],
-                    'number': row[5],
-                    'diagnosis': row[6],
-                    'doctor': row[7]
+                    'Name': row[0],
+                    'CertNo': row[1],
+                    'vaccineName': row[2],
+                    'Id': row[3],
+                    'FirstVaccineDate': row[4],
+                    'SecondVaccineDate': row[5]
                 }
                 info_list.append(row_dict)
                 
@@ -52,23 +73,99 @@ def get_info():
     conn.close()
     return got_info
 
-def add_info(info):
+def get_upcomingDoseInfo():
     conn = open_connection()
     with conn.cursor() as cursor:
-        cursor.execute('INSERT INTO info (name ,gender,age,address,number,diagnosis,doctor) VALUES(%s,%s,%s,%s,%s,%s,%s)', (info["name"], info["gender"], info["age"],info["address"],info["number"],info["diagnosis"],info["doctor"]))
-    conn.commit()
+        result = cursor.execute('SELECT * FROM upcoming_dose;')
+        info = cursor.fetchall()
+        if result > 0:
+            # Convert the results to a list of dictionaries
+            info_list = []
+            for row in info:
+                row_dict = {
+                    'SId': row[0],
+                    'Sname': row[1],
+                    'Second_Dose_dates': row[2]
+                }
+                info_list.append(row_dict)
+                
+            got_info = json.loads(json.dumps(info_list))
+        else:
+            got_info = 'No info in DB'
     conn.close()
+    return got_info
 
-def update_info(patient_id, new_info):
-    conn = open_connection()
-    with conn.cursor() as cursor:
-        cursor.execute('UPDATE info SET name=%s, gender=%s, age=%s, address=%s, number=%s, diagnosis=%s, doctor=%s WHERE PatientID=%s', (new_info["name"], new_info["gender"], new_info["age"], new_info["address"], new_info["number"], new_info["diagnosis"], new_info["doctor"], patient_id))
-    conn.commit()
-    conn.close()
 
-def delete_info(patient_id):
+def get_diagnosedInfo():
     conn = open_connection()
     with conn.cursor() as cursor:
-        cursor.execute('DELETE FROM info WHERE PatientID=%s', (patient_id,))
-    conn.commit()
+        result = cursor.execute('SELECT * FROM Diagnosed;')
+        info = cursor.fetchall()
+        if result > 0:
+            # Convert the results to a list of dictionaries
+            info_list = []
+            for row in info:
+                row_dict = {
+                    'RTPCRId': row[0],
+                    'EpId': row[1],
+                    'LeavingDate': row[2],
+                    'Hospital': row[3],
+                    'quarantine_days': row[4],
+                }
+                info_list.append(row_dict)
+                
+            got_info = json.loads(json.dumps(info_list))
+        else:
+            got_info = 'No info in DB'
     conn.close()
+    return got_info
+
+
+
+# def get_info():
+#     conn = open_connection()
+#     with conn.cursor() as cursor:
+#         result = cursor.execute('SELECT * FROM info;')
+#         info = cursor.fetchall()
+#         if result > 0:
+#             # Convert the results to a list of dictionaries
+#             info_list = []
+#             for row in info:
+#                 row_dict = {
+#                     'id': row[0],
+#                     'name': row[1],
+#                     'gender': row[2],
+#                     'age': row[3],
+#                     'address': row[4],
+#                     'number': row[5],
+#                     'diagnosis': row[6],
+#                     'doctor': row[7]
+#                 }
+#                 info_list.append(row_dict)
+                
+#             got_info = json.loads(json.dumps(info_list))
+#         else:
+#             got_info = 'No info in DB'
+#     conn.close()
+#     return got_info
+
+# def add_info(info):
+#     conn = open_connection()
+#     with conn.cursor() as cursor:
+#         cursor.execute('INSERT INTO info (name ,gender,age,address,number,diagnosis,doctor) VALUES(%s,%s,%s,%s,%s,%s,%s)', (info["name"], info["gender"], info["age"],info["address"],info["number"],info["diagnosis"],info["doctor"]))
+#     conn.commit()
+#     conn.close()
+
+# def update_info(patient_id, new_info):
+#     conn = open_connection()
+#     with conn.cursor() as cursor:
+#         cursor.execute('UPDATE info SET name=%s, gender=%s, age=%s, address=%s, number=%s, diagnosis=%s, doctor=%s WHERE PatientID=%s', (new_info["name"], new_info["gender"], new_info["age"], new_info["address"], new_info["number"], new_info["diagnosis"], new_info["doctor"], patient_id))
+#     conn.commit()
+#     conn.close()
+
+# def delete_info(patient_id):
+#     conn = open_connection()
+#     with conn.cursor() as cursor:
+#         cursor.execute('DELETE FROM info WHERE PatientID=%s', (patient_id,))
+#     conn.commit()
+#     conn.close()
--- main.py
+++ main.py
@@ -1,44 +1,53 @@
 
 from flask import Flask, jsonify, request,render_template
-from db import get_info, add_info,update_info, delete_info
+#from db import get_info, add_info,update_info, delete_info
+from db import get_info, insert_diagnosed,insert_regularCheckup,insert_vaccination,get_upcomingDoseInfo,get_diagnosedInfo,get_upcomingDoseInfo,get_diagnosedInfo
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET','PUT','DELETE'])
+@app.route('/regularCheckup', methods=['POST'])
 def info():
     if request.method == 'POST':
         if not request.is_json:
             return jsonify({"msg": "Missing JSON in request"}), 400  
 
-        add_info(request.get_json())
+        insert_regularCheckup(request.get_json())
         return 'Info Added'
     
-    if request.method == 'PUT':
+@app.route('/vaccination', methods=['POST'])
+def info():
+    if request.method == 'POST':
         if not request.is_json:
-            return jsonify({"msg": "Missing JSON in request"}), 400
-        
-        payload = request.get_json()
-        patient_id = payload.pop('PatientID', None)
-        if patient_id is None:
-            return jsonify({"msg": "Missing PatientID in request"}), 400
-        update_info(patient_id, payload)
-        return 'Info Updated'
-    
-    if request.method == 'DELETE':
-        if not request.is_json:
-            return jsonify({"msg": "Missing JSON in request"}), 400
-        
-        payload = request.get_json()
-        patient_id = payload.get('PatientID')
-        if patient_id is None:
-            return jsonify({"msg": "Missing PatientID in request"}), 400
-        delete_info(patient_id)
-        return 'Info Deleted'
+            return jsonify({"msg": "Missing JSON in request"}), 400  
 
-@app.route('/view')
+        insert_vaccination(request.get_json())
+        return 'Info Added'
+
+@app.route('/diagnosed', methods=['POST'])
+def info():
+    if request.method == 'POST':
+        if not request.is_json:
+            return jsonify({"msg": "Missing JSON in request"}), 400  
+
+        insert_diagnosed(request.get_json())
+        return 'Info Added'
+
+    
+
+@app.route('/info')
 def view():
     info = get_info()
-    return render_template('view.html', data=info)   
+    return render_template('viewVaccine.html', data=info)   
+
+@app.route('/diagnosedDetails')
+def view():
+    info = get_info()
+    return render_template('viewDiagnosed.html', data=info)   
+
+@app.route('/analytics')
+def view():
+    info = get_info()
+    return render_template('viewAnalytics.html', data=info)   
 
 if __name__ == '__main__':
     app.run()
